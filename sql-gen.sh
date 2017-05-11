6bffdd51fd4819e0d14b0da57f28276a99ae776b
Caleb Smith <karagenit@outlook.com>
2017-05-10 20:28:02 -0400

    SQL Generate Part of Speech

--- sql-gen.sh
+++ sql-gen.sh
@@ -34,17 +34,22 @@ do
 		if [ "$value" == "word" ]
 		then
 			parse=`echo $field | sed -E "s/[#]{0,}([A-Za-z.-]{1,})[,]{0,}/\\1/"`
+			# odd behavior note: with [A-Za-z.] instead, it still keeps -, entries, AND keeps the ,
 			word="$word $parse"
 				
 			if [[ $field != *","* ]]
 			then
 				value=pos
 			fi
+		elif [ "$value" == "pos" ]
+		then
+			pos=$field
+			value=decl
 		fi
 	done
 
 	#echo $word
-	add-entry "$word" 'test' 'test' 'test' 'test'
+	add-entry "$word" "$pos" 'test' 'test' 'test'
 
 	#echo "$word \t $wtype \t $mod \t $def"
 	# TODO set empy vals to NULL
